6cb69c0eb4a84c157b55c52647d8e75446cb4858
Efthymios Kalyviotis <ekalyvio@gmail.com>
2020-12-19 08:51:31 +0200

    Version 3 update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 80
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.write('Hello World\n')
-  response.write('from version 2\n')
+  response.write('from version 3\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
